56527f408dc6d0caf346093b958cde523d993acb
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-03-13 11:17:32 +0100

    clean c

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handleClick = (event) => {
+    event.preventDefault()
+    axios.get('/greeting')
+    .then((res) => {
+      console.log(res)   
+    })
+      .catch((err) => console.log(err))
+}
+
   return (
     <div className="App">
       <h1>React here!!</h1>
+     
+ <button onClick={handleClick}>Click</button>     
     </div>
   );
 }
